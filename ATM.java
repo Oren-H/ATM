96b7b3ccd1b94ce848b6d49ac838482b753b0f57
Oren Hartstein <orenh2020@gmail.com>
2023-08-31 14:58:51 -0700

    Added openAccount method

--- ATM.java
+++ ATM.java
@@ -1,9 +1,18 @@
 import java.util.HashMap;
 
-public class ATM{
+public class ATM {
     private HashMap <String, Double> accounts = new HashMap<>(); 
 
     public ATM (HashMap<String, Double> accounts){
         this.accounts = accounts;
+    }
+
+    public void openAccount(String userId, double amount) throws Exception{
+        if(!accounts.containsKey(userId)){
+            accounts.put(userId, amount);
+        }
+        else{
+            throw new Exception("User already exists");
+        }
     }
 }
